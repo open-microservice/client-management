ebc97de8b53ebb9b9e7a00b614ed84fcd294555f
George Hanson <george.hanson@htk.co.uk>
2019-05-14 10:15:27 +0100

    Add contact->client relationship

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Contact extends Model
 {
     use HasIdentifier;
+
+    public function client()
+    {
+        return $this->belongsTo(Client::class);
+    }
 }
